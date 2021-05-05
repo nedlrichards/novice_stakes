581831de6bd6d769845550c9b2867c84b114105e
Ned Richards <edwardlrichards@gmail.com>
2021-05-05 08:07:00 -0700

    seems like accelerated spectral sum is good enough

--- novice_stakes/periodic_scatter/spectral.py
+++ novice_stakes/periodic_scatter/spectral.py
@@ -1,9 +1,9 @@
 import numpy as np
 from math import pi
 from mpmath import mp
-from scipy.special import factorial, erfc
+from scipy.special import factorial, erfc, kn
 
-def define_ms(kcL, alpha_0L, dx_L, dz_L, num_eva):
+def define_ms(kcL, alpha_0L, dx_L, num_eva):
     """calculate a spectral sum vector based on number of evanescent terms"""
     kcL = np.real(kcL)
     num_p = np.fix((kcL - alpha_0L) / (2 * pi)) + num_eva
@@ -19,7 +19,7 @@ def define_ms(kcL, alpha_0L, dx_L, dz_L, num_eva):
 def G_spec(kcL, alpha_0L, dx_L, dz_L, num_eva):
     """naive implimentation of spectral representation"""
     adz = np.abs(dz_L)
-    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, dz_L, num_eva)
+    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, num_eva)
     G = np.exp(-gamma_mL * adz + 1j * alpha_mL * dx_L) / gamma_mL
 
     return -G.sum() / 2
@@ -29,7 +29,7 @@ def G_spec_Kummar(kcL, alpha_0L, dx_L, dz_L, num_eva):
     adz = np.abs(dz_L)
     gamma_0L = -1j * np.sqrt(kcL ** 2 - alpha_0L ** 2 + 0j)
 
-    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, dz_L, num_eva)
+    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, num_eva)
     # remove m==0
     mi = ms != 0
     ms = ms[mi]
@@ -56,36 +56,3 @@ def G_spec_Kummar(kcL, alpha_0L, dx_L, dz_L, num_eva):
     G = np.exp(-gamma_0L * adz) / gamma_0L + complex(S) + G.sum()
     G *= -np.exp(1j * alpha_0L * dx_L) / 2
     return G
-
-def G_Ewald(L, kc, theta_inc, dx, dz, sum_num, n_max, a=2):
-    """Accelerate periodic greens function estimate with Ewald sum"""
-    K = 2 * pi / L
-    adz = np.abs(dz)
-    alpha_0 = kc * np.cos(theta_inc)
-    ms = np.arange(-sum_num, sum_num + 1)
-    ns = np.arange(n_max + 1)
-    alpha_m = alpha_0 + ms * K
-    gamma_m = -1j * np.sqrt(kc ** 2 - alpha_m ** 2 + 0j)
-    #gamma_m = np.sqrt(kc ** 2 - alpha_m ** 2 + 0j)
-    r_m = np.sqrt((dx + ms * L) ** 2 + dz ** 2)
-
-    G1 = np.exp(gamma_m * adz) * erfc(gamma_m * L / (2 * a) + a * adz / L) \
-       + np.exp(-gamma_m * adz) * erfc(gamma_m * L / (2 * a) - a * adz / L)
-    #G1 = np.exp(-1j * gamma_m * adz) * erfc(a * adz - 1j * gamma_m / (2 * a)) \
-       #+ np.exp(1j * gamma_m * adz) * erfc(-a * adz - 1j * gamma_m / (2 * a))
-
-    G1 *= np.exp(1j * alpha_m * dx) / gamma_m
-    #G1 = G1.sum() * 1j / (4 * L)
-    G1 = -G1.sum() / (4 * L)
-
-    v_exp = np.vectorize(lambda *a: float(mp.expint(*a)))
-
-    #G2 = (kc / (2 * a)) ** (2 * ns) / factorial(ns, exact=True) \
-       #* v_exp(ns + 1, (a * r_m[:, None]) ** 2)
-    G2 = (kc * L / (2 * a)) ** (2 * ns) / factorial(ns, exact=True) \
-       * v_exp(ns + 1, (a * r_m[:, None] / L) ** 2)
-    G2 = G2.sum(axis=-1) * np.exp(1j * ms * alpha_0 * L)
-    G2 = -G2.sum() / (4 * pi)
-    import ipdb; ipdb.set_trace()
-
-    return G1 + G2
--- novice_stakes/periodic_scatter/test_spectral.py
+++ novice_stakes/periodic_scatter/test_spectral.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import pi
 from scipy.special import hankel1
 import matplotlib.pyplot as plt
-from spectral import G_spec, G_spec_Kummar, G_Ewald
+from spectral import G_spec, G_spec_Kummar, G_dz0
 
 plt.ion()
 
@@ -24,8 +24,22 @@ dz = rrcr[1] - rsrc[1]
 alpha_0 = kc * np.cos(theta_inc)
 
 ref = G_spec_Kummar(kc * L, alpha_0 * L, dx / L, dz / L, 10000)
-print(ref)
-print(G_Ewald(L, kc, theta_inc, dx, dz, 5, 5, a=2))
+
+# test of dz0
+dx_tmp = 7.
+test1_dz0 = G_dz0(kc * L, alpha_0 * L, dx_tmp / L, 10000, 20)
+test2_dz0 = G_spec_Kummar(kc * L, alpha_0 * L, dx_tmp / L, 0, 10000)
+
+n_test = [10., 25., 50., 100., 250., 500., 750., 1000., 2000., 3000., 4000., 5000.]
+G_1 = []
+G_2 = []
+for n in n_test:
+    G_1.append(G_dz0(kc * L, alpha_0 * L, dx_tmp / L, n, 10))
+    G_2.append(G_spec_Kummar(kc * L, alpha_0 * L, dx_tmp / L, 0, n))
+
+fig, ax = plt.subplots()
+ax.semilogy(n_test, np.abs(G_1 - test2_dz0))
+ax.semilogy(n_test, np.abs(G_2 - test2_dz0))
 
 1/0
 

4ee86fad016efdba9cf6c4f45787e7e807761ef0
Ned Richards <edwardlrichards@gmail.com>
2021-05-05 12:19:50 -0700

    exploring lattice sums for treating singularity

--- novice_stakes/periodic_scatter/spectral.py
+++ novice_stakes/periodic_scatter/spectral.py
@@ -1,9 +1,10 @@
 import numpy as np
 from math import pi
 from mpmath import mp
+from numexpr import evaluate
 from scipy.special import factorial, erfc, kn
 
-def define_ms(kcL, alpha_0L, dx_L, num_eva):
+def define_ms(kcL, alpha_0L, num_eva, include0=True):
     """calculate a spectral sum vector based on number of evanescent terms"""
     kcL = np.real(kcL)
     num_p = np.fix((kcL - alpha_0L) / (2 * pi)) + num_eva
@@ -14,45 +15,85 @@ def define_ms(kcL, alpha_0L, dx_L, num_eva):
     alpha_mL = alpha_0L + 2 * pi * ms
     gamma_mL = -1j * np.sqrt(kcL ** 2 - alpha_mL ** 2 + 0j)
 
+    if not include0:
+        # remove m==0
+        mi = ms != 0
+        ms = ms[mi]
+        alpha_mL = alpha_mL[mi]
+        gamma_mL = gamma_mL[mi]
+
+    # Assume last axis for summation
+    ms = ms[None, None, :]
+    alpha_mL = alpha_mL[None, None, :]
+    gamma_mL = gamma_mL[None, None, :]
+
     return ms, alpha_mL, gamma_mL
 
-def G_spec(kcL, alpha_0L, dx_L, dz_L, num_eva):
+def compute_differences(rs_L):
+    """compute distances between points"""
+    if rs_L.shape[0] != 2:
+        raise(ValueError('First dimension of rs should have dimension 2'))
+    rL_diff = rs_L[:, None, :] - rs_L[:, :, None]
+    # append an axis for summation
+    rL_diff = rL_diff[:, :, :, None]
+    dx = rL_diff[0]
+    adz = np.abs(rL_diff[1])
+    return dx, adz
+
+def G_spec(kcL, alpha_0L, rs_L, num_eva, n=None):
     """naive implimentation of spectral representation"""
-    adz = np.abs(dz_L)
-    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, num_eva)
-    G = np.exp(-gamma_mL * adz + 1j * alpha_mL * dx_L) / gamma_mL
+    dx, adz = compute_differences(rs_L)
+    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, num_eva)
+    #G = np.exp(-gamma_mL * adz + 1j * alpha_mL * rL_diff[0]) / gamma_mL
+    G = evaluate("exp(-gamma_mL * adz + 1j * alpha_mL * dx) / gamma_mL")
 
-    return -G.sum() / 2
+    # take termwise normal derivative if a normal derivative vector is
+    # specified
+    if n is not None:
+        g_vec = np.array([-np.sign(dz_L) * gamma_mL, 1j * alpha_mL])
 
-def G_spec_Kummar(kcL, alpha_0L, dx_L, dz_L, num_eva):
+    # compute spectral sum, approximate main diagonal as 0
+    # TODO: properly treat singularity at 0
+    G = -G.sum(axis=-1) / 2
+    G[np.diag_indices_from(G)] = 0. + 0.j
+
+    return G
+
+def G_spec_Kummar(kcL, alpha_0L, rs_L, num_eva):
     """kummar implimentation of spectral representation"""
-    adz = np.abs(dz_L)
+    dx, adz = compute_differences(rs_L)
+
+    # treat m=0 as a special case
     gamma_0L = -1j * np.sqrt(kcL ** 2 - alpha_0L ** 2 + 0j)
+    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, num_eva, include0=False)
+    sign_ms = np.sign(ms)
 
-    ms, alpha_mL, gamma_mL = define_ms(kcL, alpha_0L, dx_L, num_eva)
-    # remove m==0
-    mi = ms != 0
-    ms = ms[mi]
-    alpha_mL = alpha_mL[mi]
-    gamma_mL = gamma_mL[mi]
+    # spectral sum with aymptotics removed
+    u_m = evaluate("""(exp(-(2 * pi * abs(ms) + sign_ms * alpha_0L) * adz)
+                      / (2 * pi * abs(ms))) \
+                      * (1 - alpha_0L / (2 * pi * ms) + kcL ** 2 * adz / (4 * pi * abs(ms)))""")
+    G1 = evaluate("(exp(-gamma_mL * adz) / gamma_mL - u_m) * exp(2j * pi * ms * dx)")
+    G1 = G1.sum(axis=-1)
 
-    u_m = np.exp(-(2 * pi * np.abs(ms) + np.sign(ms) * alpha_0L) * adz)
-    u_m /= 2 * pi * np.abs(ms)
-    u_m *= 1 - alpha_0L / (2 * pi * ms) + kcL ** 2 * adz / (4 * pi * np.abs(ms))
+    # asymptotics of spectral sum
+    dx = dx[:, :, 0]
+    adz = adz[:, :, 0]
 
-    Z = adz + 1j * dx_L
+    Z = adz + 1j * dx
     Zc = np.conj(Z)
+    argZ = evaluate("exp(-2 * pi * Z)") + np.spacing(1)
+    argZc = np.conj(argZ)
 
-    S = np.exp(-alpha_0L * adz) \
-      * (mp.polylog(1, np.exp(-2 * pi * Zc)) / (2 * pi)
-         - (2 * alpha_0L - kcL ** 2 * adz) * mp.polylog(2, np.exp(-2 * pi * Zc)) \
-            / (8 * pi ** 2))
-    S += np.exp(alpha_0L * adz) \
-      * (mp.polylog(1, np.exp(-2 * pi * Z)) / (2 * pi)
-         + (2 * alpha_0L + kcL ** 2 * adz) * mp.polylog(2, np.exp(-2 * pi * Z)) \
-            / (8 * pi ** 2))
+    v_poly = np.vectorize(lambda *a: complex(mp.polylog(*a)))
 
-    G = (np.exp(-gamma_mL * adz) / gamma_mL - u_m) * np.exp(2j * pi * ms * dx_L)
-    G = np.exp(-gamma_0L * adz) / gamma_0L + complex(S) + G.sum()
-    G *= -np.exp(1j * alpha_0L * dx_L) / 2
+    S = evaluate("exp(-alpha_0L * adz)") * (v_poly(1, argZc) / (2 * pi)
+          - evaluate("2 * alpha_0L - kcL ** 2 * adz") \
+          * v_poly(2, argZc) / (8 * pi ** 2))
+    S += evaluate("exp(alpha_0L * adz)") * (v_poly(1, argZ) / (2 * pi)
+         + evaluate("2 * alpha_0L + kcL ** 2 * adz") \
+         * v_poly(2, argZ) / (8 * pi ** 2))
+
+    G = (evaluate("exp(-gamma_0L * adz) / gamma_0L") + S) + G1
+    G *= evaluate("-exp(1j * alpha_0L * dx) / 2")
+    G[np.diag_indices_from(G)] = 0. + 0.j
     return G
--- novice_stakes/periodic_scatter/test_spectral.py
+++ novice_stakes/periodic_scatter/test_spectral.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import pi
 from scipy.special import hankel1
 import matplotlib.pyplot as plt
-from spectral import G_spec, G_spec_Kummar, G_dz0
+from spectral import G_spec, G_spec_Kummar
 
 plt.ion()
 
@@ -14,33 +14,14 @@ kc = 2 * pi * fc / c
 
 L = 70.
 
-rsrc = np.array([0.3, 0.])
-#rrcr = np.array([0.3, .01])
-rrcr = np.array([0.3, .01])
-
-dx = rrcr[0] - rsrc[0]
-dz = rrcr[1] - rsrc[1]
+dx = 0.1
+x_axis = np.arange(10) * dx
+z = np.sin(x_axis * 2 * pi)
+r_axis = np.array([x_axis, z])
 
 alpha_0 = kc * np.cos(theta_inc)
-
-ref = G_spec_Kummar(kc * L, alpha_0 * L, dx / L, dz / L, 10000)
-
-# test of dz0
-dx_tmp = 7.
-test1_dz0 = G_dz0(kc * L, alpha_0 * L, dx_tmp / L, 10000, 20)
-test2_dz0 = G_spec_Kummar(kc * L, alpha_0 * L, dx_tmp / L, 0, 10000)
-
-n_test = [10., 25., 50., 100., 250., 500., 750., 1000., 2000., 3000., 4000., 5000.]
-G_1 = []
-G_2 = []
-for n in n_test:
-    G_1.append(G_dz0(kc * L, alpha_0 * L, dx_tmp / L, n, 10))
-    G_2.append(G_spec_Kummar(kc * L, alpha_0 * L, dx_tmp / L, 0, n))
-
-fig, ax = plt.subplots()
-ax.semilogy(n_test, np.abs(G_1 - test2_dz0))
-ax.semilogy(n_test, np.abs(G_2 - test2_dz0))
-
+ref = G_spec_Kummar(kc * L, alpha_0 * L, r_axis / L, 10000)
+test = G_spec(kc * L, alpha_0 * L, r_axis / L, 10000)
 1/0
 
 n_test = [10., 25., 50., 100., 250., 500., 750., 1000., 2000., 3000., 4000., 5000.]
